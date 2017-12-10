b7ca031a8847b3eebf8fcb8023dd113caf866522
Xinbin <xbhuang93@hotmail.com>
2017-12-10 13:36:00 -0800

    Edit and add documentation to "download-data.R"

--- src/download-data.R
+++ src/download-data.R
@@ -1,5 +1,28 @@
-## download the data
-download.file(url = "https://raw.githubusercontent.com/xinbinhuang/data-bitcoin/master/bitcoin_dataset.csv",
-              destfile = "../data/bitcoin_dataset.csv", method = "curl")
-download.file(url = "https://raw.githubusercontent.com/xinbinhuang/data-bitcoin/master/bitcoin_price.csv",
-              destfile = "../data/bitcoin_price.csv", method = "curl")
+#! /usr/bin/env Rscript 
+# download-data.R
+# Xinbin Huang, 2017
+#
+# This script downloads the required bitcoin data for this analysis. 
+# The downloaded data is located in the `data` folder. This script takes no argument.
+#
+# Usage: Rscript download-data.R
+
+# define main function
+main <- function(){
+      
+      # download "bitcoin_dataset.csv"
+      if(file.exists(!"../data/bitcoin_dataset.csv"))
+      {download.file(url = "https://raw.githubusercontent.com/xinbinhuang/data-bitcoin/master/bitcoin_dataset.csv",
+                     destfile = "../data/bitcoin_dataset.csv", method = "curl")}
+      
+      # download "bitcoin_price.csv"
+      if(file.exists(!"../data/bitcoin_price.csv"))
+      {download.file(url = "https://raw.githubusercontent.com/xinbinhuang/data-bitcoin/master/bitcoin_price.csv",
+                     destfile = "../data/bitcoin_price.csv", method = "curl")}
+}
+
+# call main function
+main()
+
+
+
