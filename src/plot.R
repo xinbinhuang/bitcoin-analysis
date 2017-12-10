a203f561b88268e1dfb0023cafdfff1125c417c2
Xinbin <xbhuang93@hotmail.com>
2017-12-10 14:50:11 -0800

    Add plot.R script

---
+++ src/plot.R
@@ -0,0 +1,35 @@
+#! /usr/bin/env Rscript 
+# plot.R
+# Xinbin Huang, 2017
+#
+# This output the pair-plots of the three variables
+#
+# This should take two arguments
+# - data:         a path pointing to the data
+# - output:       a path/filename where to write the figure to and what to call it 
+#
+# Usage: Rscript src/plot.R results/merged-data.csv results/figure/analysis-plot.png
+
+# read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+data <- args[1]
+output <- args[2]
+
+# define main function
+main <- function(){
+      
+      # loading packages
+      suppressPackageStartupMessages(library(GGally))
+      suppressPackageStartupMessages(library(tidyverse))
+      
+      # read in the data frame
+      bitcoin <- read_csv(data)
+      
+      # plot pair-plot
+      p <- ggpairs(bitcoin) + ggtitle("Pair plot of the three variables")
+
+      # output the data to figure folder
+      ggsave(output, plot = p, device = "png")
+}
+
+main()
