acc31c7f8443f7fbba11a3c2b1a6da97ad08e33a
Xinbin <xbhuang93@hotmail.com>
2017-12-10 14:34:53 -0800

    Add descriptive analysis script

---
+++ src/descriptive.R
@@ -0,0 +1,39 @@
+#! /usr/bin/env Rscript 
+# descriptive.R
+# Xinbin Huang, 2017
+#
+# This script gives the descriptive analysis for the variables `Close`, `Volume`, 
+# and `measure of difficulty to mine a new block`. 
+# 
+# This should take two arguments
+# - data:         a path pointing to the data
+# - output:       a path/filename where to write the file to and what to call it (i.e. results/descriptive-result.csv)
+#
+# Usage: Rscript src/descriptive.R results/merged-data.csv results/descriptive-result.csv
+
+# read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+data <- args[1]
+output <- args[2]
+
+# define main function
+main <- function(){
+      
+      # loading packages
+      suppressPackageStartupMessages(library(broom))
+      suppressPackageStartupMessages(library(tidyverse))
+      
+      # load in the data
+      bitcoin <- read_csv("results/merged-data.csv")
+      descrptive <- broom::tidy(summary(bitcoin))
+      
+      ## output the result to results folder
+      write_csv(descrptive, output)
+}
+
+# call main function
+main()
+
+
+
+
